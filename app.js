1b2e61d04af6e6428ecdfb45fa834be2a54d7eab
Krishna Malviya <malviyakrishna1108@gmail.com>
2025-07-11 16:12:59 +0530

    phase1 done

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
-
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js")
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -33,6 +34,16 @@ app.get("/", (req, res) => {
   res.send("Hi, i am root");
 });
 
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
 //index route
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({});
@@ -53,14 +64,24 @@ app.get("/listings/:id", async (req, res) => {
 
 //Create Route
 app.post(
-  "/listings", 
-  wrapAsync (async ( req, res, next) => {
-
+  "/listings",
+  (req, res, next) => {
+    // Convert image string to object early
+    const img = req.body.listing.image;
+    req.body.listing.image = {
+      url: img || undefined,
+      filename: "listingimage"
+    };
+    next();
+  },
+  validateListing,
+  wrapAsync(async (req, res) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listings")
-})
-)
+    res.redirect("/listings");
+  })
+);
+
 
 
 //Edit Route
@@ -71,11 +92,14 @@ app.get("/listings/:id/edit", async (req, res) => {
 });
 
 //Update Route
-app.put("/listings/:id", async (req, res) => {
-  let { id } = req.params;
-  await Listing.findByIdAndUpdate(id, { ...req.body.listing });
-  res.redirect(`/listings/${id}`);
-});
+app.put("/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
 
 //Delete Route
 app.delete("/listings/:id", async (req, res) => {
@@ -99,8 +123,16 @@ app.delete("/listings/:id", async (req, res) => {
 //     res.send("Testing successful");
 //   });
 
+app.use((req, res, next) => {
+  console.log("Request Path:", req.path);
+  next(new ExpressError(404, "Page Not Found!"));
+});
+
+
 app.use((err, req, res, next) => {
-  res.send("something went wrong")
+  let { statusCode = 500, message = "Something went wrong" } = err;
+  res.status(statusCode).render("error.ejs", { err });
+  console.log(err)
 })
 
 app.listen(8080, () => {
