ee0b2c41addc539dc8883d4207dacb933af596c7
Krishna Malviya <malviyakrishna1108@gmail.com>
2025-07-17 15:38:12 +0530

    styling

--- routes/listing.js
+++ routes/listing.js
@@ -5,48 +5,45 @@ const wrapAsync = require("../utils/wrapAsync.js"); // import wrapAsync function
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    (req, res, next) => {
+      // Convert image string to object early
+      const img = req.body.listing.image;
+      req.body.listing.image = {
+        url: img || undefined,
+        filename: "listingimage"
+      };
+      next();
+    },
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
+
+  //New Route
+router.get("/new", isLoggedIn, listingController.renderNewForn);
 
 
-//index route
-router.get("/",wrapAsync(listingController.index) );
 
-//New Route
-router.get("/new", isLoggedIn, listingController.renderNewForn );
-
-//Show Route
-router.get("/:id", wrapAsync(listingController.showListing));
-
-//Create Route
-router.post(
-  "/",
-  isLoggedIn,
-  (req, res, next) => {
-    // Convert image string to object early
-    const img = req.body.listing.image;
-    req.body.listing.image = {
-      url: img || undefined,
-      filename: "listingimage"
-    };
-    next();
-  },
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
+router
+  .route("/:id")
+  .get( wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete( isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 
 
 //Edit Route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm));
 
-//Update Route
-router.put("/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing)
-);
 
-//Delete Route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 module.exports = router;
